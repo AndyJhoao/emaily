3767005c131a4d5b5442e46a27b652095d6adb4c
Andy Jhoao <andy_joao@live.com>
2022-07-20 22:43:14 -0500

    change greeting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-    res.send({hi:'there'})
+    res.send({bye:'buddy'})
 });
 
-app.listen(port);
+app.listen(PORT);
