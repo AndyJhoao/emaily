4c32836eeab9562fce3c70c64bd70dd42aca985d
Andy Jhoao <andy_joao@live.com>
2022-08-01 23:03:44 -0500

    config heroku variables env

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
-
-// googleClientID:'417413743227-loqk51v4rk0qbh291c5r2tss0upa3e3f.apps.googleusercontent.com',
-// googleClientSecret:'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',
-// mongoURI:'mongodb+srv://andyjhoao:4sKqEh8e09n2g9nF@email-cluster-app.ruidqk6.mongodb.net/?retryWrites=true&w=majority',
-// cookieKey: 'asdertfghbgqwdfcplxigjmn'
