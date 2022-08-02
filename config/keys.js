4167e5bed664bc4149f5fbb90d5be3a4aed59f74
Andy Jhoao <andy_joao@live.com>
2022-08-01 22:53:35 -0500

    keys prod and dev

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require("./prod")
+}else{
+    module.exports = require("./dev")
+}
+//23axqAAEPG91HYK6 password cluster dev
+
+//4sKqEh8e09n2g9nF password cluster prod
+
+
---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
+
+// googleClientID:'417413743227-loqk51v4rk0qbh291c5r2tss0upa3e3f.apps.googleusercontent.com',
+// googleClientSecret:'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',
+// mongoURI:'mongodb+srv://andyjhoao:4sKqEh8e09n2g9nF@email-cluster-app.ruidqk6.mongodb.net/?retryWrites=true&w=majority',
+// cookieKey: 'asdertfghbgqwdfcplxigjmn'
